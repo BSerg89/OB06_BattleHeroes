3190268e8bde5bd7ca4c2c267a761054c9971646
BSerg89 <barkin.s.m@gmail.com>
2024-09-08 20:29:22 +0700

    add code game

---
+++ OB06_BattleHeroes.py
@@ -0,0 +1,59 @@
+from faker import Faker
+import random
+
+
+# Класс Hero
+class Hero:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.attack_power = 20
+
+    def attack(self, other):
+        damage = random.randint(10, self.attack_power)
+        other.health -= damage
+        print(f"{self.name} атаковал {other.name} и нанёс {damage} урона!")
+
+    def is_alive(self):
+        return self.health > 0
+
+    def __str__(self):
+        return f"{self.name}: Здоровье = {self.health}"
+
+
+# Класс Game
+class Game:
+    def __init__(self, player_name):
+        fake = Faker()  # Создаём экземпляр Faker для генерации имени
+        computer_name = fake.first_name() + "(Comp)"  # Генерация случайного имени для компьютера
+        self.player = Hero(player_name)
+        self.computer = Hero(computer_name)
+
+    def start(self):
+        print("Игра началась!")
+        while self.player.is_alive() and self.computer.is_alive():
+            # Ход игрока
+            self.player.attack(self.computer)
+            if not self.computer.is_alive():
+                print(f"{self.computer.name} повержен. {self.player.name} победил!")
+                break
+
+            # Ход компьютера
+            self.computer.attack(self.player)
+            if not self.player.is_alive():
+                print(f"{self.player.name} повержен. {self.computer.name} победил!")
+                break
+
+            # Вывод текущего состояния здоровья
+            print(self.player)
+            print(self.computer)
+            print("-" * 30)
+
+        print("Игра окончена!")
+
+
+# Основная программа
+if __name__ == "__main__":
+    player_name = input("Введите имя своего героя: ")
+    game = Game(player_name)
+    game.start()
